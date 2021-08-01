3fc4d14354a00d6786504ed7677837bf739cf3dd
ihtisham <ihtisham@ihtisham.com>
2021-08-01 12:39:40 +0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    
     extend: {
      height:{
-       '32':'218px'
-     }
+       '32':'270px',
+       '42':'330px'
+     },
+     margin:{
+      '12':'12em',
+      '42':'330px',
+      "80":"21rem",
+      "96":"600px"
+    },
     },
   },
   variants: {
